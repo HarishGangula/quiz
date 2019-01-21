c0bbbeb9671db804464678402325badc98a249de
ankur01oct <ankur01oct@gmail.com>
2019-01-21 18:03:47 +0530

    added qr code reader

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
+
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,25 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public scanner: BarcodeScanner, ) {
+  } 
+    options:BarcodeScannerOptions ;
+    encodText:string = '' ;
+    scannedData:any = {};
 
+
+    scan () {  
+      this.options = {
+        prompt : 'Scan your QRCode'
+      };
+
+    this.scanner.scan().then(scannedData => {
+      this.scannedData = scannedData ;
+      console.log('Barcode data', scannedData);
+     }).catch(err => {
+         console.log('Error', err);
+     })
+  
+    }
   }
 
-}
